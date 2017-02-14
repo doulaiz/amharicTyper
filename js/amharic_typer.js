cb36a08dc5305e75832f4528a3dd4813b64e2062
Rafael Deleuze <rdeleuze@apposit.com>
2017-02-14 15:10:15 +0300

    Bugfix: remove space on phonem

--- js/amharic_typer.js
+++ js/amharic_typer.js
@@ -47,7 +47,7 @@
 
     translatorMap["HA"] = ["ሃ", "ሓ", "ሀ", "ሐ"];
     translatorMap["HE"] = ["ሄ", "ሔ"];
-    translatorMap["HI"] = ["ሂ ", "ሒ", "ህ", "ሕ"];
+    translatorMap["HI"] = ["ሂ", "ሒ", "ህ", "ሕ"];
     translatorMap["HO"] = ["ሆ", "ሖ"];
     translatorMap["HU"] = ["ሁ", "ሑ"];
     translatorMap["H"] = ["ህ", "ሕ"];
